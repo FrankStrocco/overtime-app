9a1739a437370def747235d6c1026f8f1065bb7f
Frank Strocco <frank.strocco@gmail.com>
2019-06-18 21:55:10 -0700

    added rspec, capybara, setup posgresql database for test and production and setup homepage

---
+++ NOTES/Rails_notes.rb
@@ -0,0 +1,29 @@
+rails new name -d database
+
+// this doesn't work anymore...
+mysql> GRANT ALL PRIVILEGES ON simple_cms_development.* TO 'rails_user'@'localhost' IDENTIFIED BY 'tempPassword';
+
+use these...
+1. create the user.
+CREATE USER 'rails_user'@'localhost' IDENTIFIED BY 'tempPassword';
+2. grant privileges to that user on each environment (development, test, production, ect..)
+GRANT ALL PRIVILEGES ON simple_cms_develpment.* TO 'rails_user'@'localhost';
+GRANT ALL PRIVILEGES ON simple_cms_test.* TO 'rails_user'@'localhost';
+
+rails g controller name name 2nd name is for it's view
+ROUTES..
+
+default route in rails, but might get depreciated soon..
+
+get ':controller(/:action(/:id))'
+
+---
+
+rspec selenium depreciated warning..
+WARN Selenium [DEPRECATION] Selenium::WebDriver::Chrome#driver_path= is deprecated. Use Selenium::WebDriver::Chrome::Service#driver_path= instead
+
+https://github.com/SeleniumHQ/selenium/issues/7125
+
+change gem ''chromedriver-helper' to ...
+
+gem 'webdrivers', '~> 3.2.0'
